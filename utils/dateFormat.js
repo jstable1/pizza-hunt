60e5949cbf69ba954bffac39a9a8f098e7dfdb5f
jstable1 <jessica.stabler@gmail.com>
2022-04-26 16:47:33 -0600

    created comment CRUD routes and linked w/front end

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -71,8 +71,8 @@ module.exports = (
 
   let hour;
   // check for 24-hr time
-  if (dateObj.getHours > 12) {
-    hour = Math.floor(dateObj.getHours() / 2);
+  if (dateObj.getHours() > 12) {
+    hour = Math.floor(dateObj.getHours() - 12);
   } else {
     hour = dateObj.getHours();
   }
@@ -81,7 +81,13 @@ module.exports = (
     hour = 12;
   }
 
-  const minutes = dateObj.getMinutes();
+  //gets the minutes
+  let minutes;
+  if (dateObj.getMinutes() < 10) {
+    minutes = ("0" + dateObj.getMinutes()).slice(-2);
+  } else {
+    minutes = dateObj.getMinutes();
+  }
 
   // set `am` or `pm`
   let periodOfDay;
